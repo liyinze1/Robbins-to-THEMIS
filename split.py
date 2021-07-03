aa563609efd67c5e6086a489a1937830a18913ce
acse-yl13320 <50014026+acse-yl13320@users.noreply.github.com>
2021-07-04 00:13:34 +0100

    Update split.py

--- split.py
+++ split.py
@@ -2,14 +2,21 @@ valid_ratio = 0.2
 
 import os
 import random
-
-os.system('move "valid/images\\*.*" "dataset/images/"')
-os.system('move "valid/labels\\*.*" "dataset/labels/"')
+import platform
 
 names = [name[: -4] for name in os.listdir('dataset/images/')]
 random.shuffle(names) 
 valid_size = int(len(names) * valid_ratio)
 
-for name in names[: valid_size]:
-    os.system('move "dataset/images\\%s.png" "valid/images/"' % name)
-    os.system('move "dataset/labels\\%s.txt" "valid/labels/"' % name)
+if platform.system().lower() == 'windows':
+    os.system('move "valid/images\\*.*" "dataset/images/"')
+    os.system('move "valid/labels\\*.*" "dataset/labels/"')
+    for name in names[: valid_size]:
+        os.system('move "dataset/images\\%s.png" "valid/images/"' % name)
+        os.system('move "dataset/labels\\%s.txt" "valid/labels/"' % name)
+elif platform.system().lower() == 'linux':
+    os.system('mv "valid/images/*.*" "dataset/images/"')
+    os.system('mv "valid/labels/*.*" "dataset/labels/"')
+    for name in names[: valid_size]:
+        os.system('mv "dataset/images/%s.png" "valid/images/"' % name)
+        os.system('mv "dataset/labels/%s.txt" "valid/labels/"' % name)
