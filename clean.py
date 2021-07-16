48b21d341e6f3d957bb7fdee0eaba4993bfe9a49
acse-yl13320 <50014026+acse-yl13320@users.noreply.github.com>
2021-07-16 22:56:04 +0100

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,5 +1,7 @@
 import os
 os.system('rm -f dataset/images/*')
+os.system('rm -f dataset/deleted_images/*')
 os.system('rm -f valid/images/*')
 os.system('rm -f dataset/labels/*')
+os.system('rm -f dataset/revised_labels/*')
 os.system('rm -f valid/labels/*')
