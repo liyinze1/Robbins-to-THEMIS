0cbc5acb520a26b1725b6b7b341775c6d6f7c111
acse-yl13320 <50014026+acse-yl13320@users.noreply.github.com>
2021-07-18 12:18:16 +0100

    update split.py

--- split.py
+++ split.py
@@ -4,6 +4,8 @@ import os
 import random
 import platform
 
+dataset_path = 'dataset'
+valid_path = 'valid'
 
 if platform.system().lower() == 'windows':
     slash = '\\'
@@ -12,16 +14,16 @@ else:
     slash = '/'
     mv_cmd = 'mv'
 
-os.system('mkdir valid')
-os.system('mkdir valid' + slash + 'images')
-os.system('mkdir valid' + slash + 'labels')
+os.system('mkdir ' + valid_path)
+os.system('mkdir ' + valid_path + slash + 'images')
+os.system('mkdir ' + valid_path + slash + 'labels')
 
 names = [name[: -4] for name in os.listdir('dataset/images/')]
 random.shuffle(names) 
 valid_size = int(len(names) * valid_ratio)
 
-os.system('%s "valid/images%s*.*" "dataset/images/"' % (mv_cmd, slash))
-os.system('%s "valid/labels%s*.*" "dataset/labels/"' % (mv_cmd, slash))
+os.system('%s "valid/images%s*.*" "%s/images/"' % (mv_cmd, slash, dataset_path))
+os.system('%s "valid/labels%s*.*" "%s/labels/"' % (mv_cmd, slash, dataset_path))
 for name in names[: valid_size]:
-    os.system('%s "dataset/images%s%s.png" "valid/images/"' % (mv_cmd, slash, name))
-    os.system('%s "dataset/labels%s%s.txt" "valid/labels/"' % (mv_cmd, slash, name))
+    os.system('%s "dataset/images%s%s.png" "%s/images/"' % (mv_cmd, slash, name, valid_path))
+    os.system('%s "dataset/labels%s%s.txt" "%s/labels/"' % (mv_cmd, slash, name, valid_path))
