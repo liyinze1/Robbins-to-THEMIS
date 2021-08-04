474071232630878be91f89875f0bd0ac28fbb23c
acse-yl13320 <50014026+acse-yl13320@users.noreply.github.com>
2021-08-04 20:29:01 +0100

    update split.py

--- split.py
+++ split.py
@@ -3,7 +3,7 @@ valid_ratio = 0.2
 import os
 import random
 
-dataset_path = 'dataset'
+dataset_path = 'combined_dataset'
 prefix_path = '../'
 
 img_list = [prefix_path + dataset_path + '/images/' + img + '\n' for img in os.listdir(dataset_path + '/images/')]
@@ -17,4 +17,7 @@ f.close()
 f = open('valid.txt', 'w')
 f.writelines(img_list[train_size:])
 f.close()
- 
+
+f = open('test.txt', 'w')
+f.writelines(img_list)
+f.close()
