62e8de3ed90777d366d537d31661da52e5241b8e
acse-yl13320 <50014026+acse-yl13320@users.noreply.github.com>
2021-08-07 01:48:25 +0100

    parser

--- split.py
+++ split.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='dataset')
 parser.add_argument('--valid-ratio', type=float, default=0.2)
-parser.add_argument('--prefix', type=str, default='../Robbins-to-THEMIS')
+parser.add_argument('--prefix', type=str, default='../Robbins-to-THEMIS/dataset')
 opt = parser.parse_args()
 
 print(opt)
@@ -14,7 +14,9 @@ dataset_path = opt.dataset
 prefix_path = opt.prefix
 valid_ratio = opt.valid_ratio
 
-img_list = [os.sep.join([prefix_path, dataset_path, 'images', img]) + '\n' for img in os.listdir(dataset_path + '/images/')]
+img_format = ('png', 'jpg', 'jpeg')
+
+img_list = [os.sep.join([prefix_path, 'images', img]) + '\n' for img in os.listdir(dataset_path + '/images/') if img.endswith(img_format)]
 random.shuffle(img_list)
 train_size = int(len(img_list) * (1 - valid_ratio))
 
